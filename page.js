046bdbd0b6b116156c90a7a15eb8d39586e534a2
Hello0903 <phi090320@gmail.com>
2021-11-22 08:10:13 +0700

    update sub

--- page.js
+++ page.js
@@ -7,7 +7,10 @@ function wassub(){
    if(localStorage.getItem('SUBCRIBER') == 1){
      $('.sub').html('Đã đăng ký');
      $('#wassub').attr('hidden',false);
-   }else $('#wassub').attr('hidden',true);
+     $('#xla').html('<a href="xla.html">Tham khảo bảng otsu matran 16*16 </a>');
+   }else{ $('#wassub').attr('hidden',true);
+     $('#xla').html('');
+        }
  }
 
 $('.sub').click(function(){
@@ -16,11 +19,13 @@ $('.sub').click(function(){
    localStorage.setItem('SUBCRIBER',0);
      $('.sub').html('Đăng ký');
      $('#wassub').attr('hidden',true);
+       $('#xla').html('');
    }
    else {localStorage.setItem('SUBCRIBER',1);
           $('.sub').html('Đã đăng ký');
           alert('Cảm ơn bạn đã đăng ký website :)');
           $('#wassub').attr('hidden',false);
+          $('#xla').html('<a href="xla.html">Tham khảo bảng otsu matran 16*16 </a>');
          }
 })
 
